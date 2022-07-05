3b11615cf9ce4657e8937972dbdf11c4d00c355d
Shan Konduru <ShanKonduru@users.noreply.github.com>
2022-07-05 18:37:15 +0530

    code changes

--- index.js
+++ index.js
@@ -4,6 +4,19 @@ const options = new chrome.Options();
 
 var chromeOptions = new chrome.Options();
 chromeOptions.addArguments("start-maximized");
+//// chromeOptions.addArguments("incognito");
+//// chromeOptions.addArguments("headless");
+
+/*
+start-maximized: Opens Chrome in maximize mode
+incognito: Opens Chrome in incognito mode
+headless: Opens Chrome in headless mode
+disable-extensions: Disables existing extensions on Chrome browser
+disable-popup-blocking: Disables pop-ups displayed on Chrome browser
+make-default-browser: Makes Chrome default browser
+version: Prints chrome browser version
+disable-infobars: Prevents Chrome from displaying the notification ‘Chrome is being controlled by automated software
+*/
 
 async function sessionTwo() {
     let driver = new webdriver.Builder()
@@ -11,7 +24,7 @@ async function sessionTwo() {
     .setChromeOptions(chromeOptions)
     .build();
 
-    await driver.get("https://www.facebook.com/");
+    await driver.get("https://www.google.com/");
 }
 
 sessionTwo();
